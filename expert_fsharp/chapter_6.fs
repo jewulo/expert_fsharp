f360240be1d1cb8918200be34a6ae99b9484d3b4
Joshua Ewulo <jewulo@gmail.com>
2026-03-07 22:14:57 +0000

    Started On Chapter 6
    Started On Chapter 6

--- expert_fsharp/chapter_6.fs
+++ expert_fsharp/chapter_6.fs
@@ -1,7 +1,15 @@
 module chapter_6
 
+    module getting_started_with_objects =
+
+        let run () = ()
+
     module execute_modules =
 
         let run () =
             printfn "[---- Expert F#: START CHAPTER 6 ----]"
+
+            getting_started_with_objects.run()
+
             printfn "[---- Expert F#: END CHAPTER 6 ----]"
+
