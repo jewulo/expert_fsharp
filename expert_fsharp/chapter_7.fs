a4eaf4c9ab39206b4a0fabf212f483e1ef4ab7bd
Joshua Ewulo <jewulo@gmail.com>
2026-03-24 20:38:18 +0000

    Add module and run function for Chapter 7
    Add module and run function for Chapter 7

--- expert_fsharp/chapter_7.fs
+++ expert_fsharp/chapter_7.fs
@@ -1,1 +1,8 @@
+module chapter_7
 
+    module execute_modules =
+
+        let run () =
+            printfn "[---- Expert F#: START CHAPTER 7 ----]"
+
+            printfn "[---- Expert F#: END CHAPTER 7 ----]"
