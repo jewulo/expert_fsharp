557d7b1bc580a8653f90675e1245756a6a72af46
Joshua Ewulo <jewulo@gmail.com>
2026-03-25 21:31:05 +0000

    Update Program.fs
    Starting Chapter 8

--- expert_fsharp/Program.fs
+++ expert_fsharp/Program.fs
@@ -1,19 +1,19 @@
-
 open System
 
 ///
-/// https://stackoverflow.com/questions/15308268/how-to-pause-the-console-in-f-language
+/// https://stackoverflow.com/questions/15308268/how-to-pause
 ///
-// When running in debug mode and using Visual Studio to run the program,  
-// one may miss the results as the program runs to the end and exists.  
-// Since running normally, i.e. Visual Studio Ctrl-F5, will add an pause
-// automatically the pause is only shown when in debug mode.  
+// When running in debug mode and using Visual Studio to run 
+// one may miss the results as the program runs to the end an
+// Since running normally, i.e. Visual Studio Ctrl-F5, will a
+// automatically the pause is only shown when in debug mode. 
 let pause () =  
     match System.Diagnostics.Debugger.IsAttached with  
     | true ->  
         printfn "\nPress any key to continue."  
         System.Console.ReadKey(true) |> ignore  
     | false -> ()  
+
 
 [<EntryPoint>]
 let main argv =
@@ -22,9 +22,10 @@ let main argv =
     //chapter_2.execute_modules.run()
     //chapter_3.execute_modules.run()
     //chapter_4.execute_modules.run()
-    chapter_5.execute_modules.run()
-    chapter_6.execute_modules.run()
-    chapter_7.execute_modules.run()
+    //chapter_5.execute_modules.run()
+    //chapter_6.execute_modules.run()
+    //chapter_7.execute_modules.run()
+    chapter_8.execute_modules.run()
 
     System.Console.ReadKey(true) |> ignore  
     pause()  
