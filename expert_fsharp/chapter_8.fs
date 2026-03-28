3e5a131e8560b0a63f8bb4914bcca6ec63b3ec92
Joshua Ewulo <jewulo@gmail.com>
2026-03-28 21:42:34 +0000

    Continue Chapter 8 Page 170
    CONTINUE FROM PAGE 170.
    USING REGULAR EXPRESSIONS TO PARSE LINES.

--- expert_fsharp/chapter_8.fs
+++ expert_fsharp/chapter_8.fs
@@ -37,13 +37,99 @@ module chapter_8
         /// CONTINUE FROM CHAPTER 8 PAGE 165
         /// USING PRINTF AND FRIENDS.
         let using_printf_and_friends () =
+            let s1 = sprintf "Name: %s, Age: %d" "Anna" 3
+            s1 |> printfn "%s"
+
+            // let s2 = sprintf "Name: %s, Age: %d" 3 10 // error FS0001: This expression was expected to have type 'string'
+
+            let s3 = System.DateTime.Now.ToString()
+            let s4 = sprintf "It is now %O" s3
+            s4 |> printfn "%s"
             ()
+
+        let generic_structural_formatting () =
+            printf "The result is %A\n" [1; 2; 3]
+
+        let formatting_strings_usning_dotnet_formatting () =
+            System.String.Format("{0} {1} {2}", 12, "a", 1.23)
 
         let run () =
             building_strings()
             more_about_string_literals()
+            using_printf_and_friends()
+            generic_structural_formatting()
+            formatting_strings_usning_dotnet_formatting()
+
+    module parsing_strings_and_textual_data =
+        
+        open System
+        open System.IO
+
+        let parsing_basic_values_1 () =
+            DateTime.Parse("13/07/1968") |> printfn "date = %A"
+
+            let date x = DateTime.Parse(x)
+            (date "13 July 1968") |> printfn "date %A"
+            (date "13 July 1968, 6:21:01pm") |> printfn "birth %A"
+
+        let parsing_basic_values_2 () =
+            Uri.TryCreate("http://www.thebritishmuseum.ac.uk/", UriKind.Absolute) |> printfn "%A"
+            Uri.TryCreate("http://e3£%.gibberish.ac.uk/", UriKind.Absolute) |> printfn "%A"
+
+        let processing_line_based_input_1 () =
+            let line = "Smith, John, 20 January 1986, Software Developer"
+            line.Split ',' |> printfn "%A"
+            line.Split ',' |> Array.map (fun s -> s.Trim())  |> printfn "%A"
+        
+        // process each line
+        let splitLine (line : string) =
+            line.Split [|','|] |> Array.map (fun s -> s.Trim())
+
+        let parseEmployee (line : string) =
+            match splitLine line with
+            | [|last; first; startDate; title|] ->
+                last, first, System.DateTime.Parse(startDate), title
+            | _ ->
+                failwithf "invalid employee format: '%s'" line
+            
+        let processing_line_based_input_2 () =
+            let line = "Smith, John, 20 January 1986, Software Developer"
+            parseEmployee line |> printfn "%A"
+
+        let processing_file_based_input () =
+            let create_1000_line_employee_file_from_record () =
+                let line = "Smith, John, 20 January 1986, Software Developer"
+                File.WriteAllLines("employees.txt", Array.create 10000 line)
+
+            create_1000_line_employee_file_from_record()
+
+            let readEmployees (fileName) =
+                fileName |> File.ReadAllLines |> Seq.map parseEmployee
+
+            let firstThree = readEmployees "employees.txt" |> Seq.truncate 3 |> Seq.toList
+
+            firstThree |> Seq.iter (fun (last, first, startDate, title) ->
+                                            printfn "%s %s started on %A" first last startDate)
+
+        let run () =
+            parsing_basic_values_1()
+            parsing_basic_values_2()
+            processing_line_based_input_1()
+            processing_line_based_input_2()
+            processing_file_based_input()
+
+    /// CONTINUE FROM PAGE 170.
+    /// USING REGULAR EXPRESSIONS TO PARSE LINES.
+    module using_regular_exprtessions_to_parse_files =
+
+        open System.Text.RegularExpressions
+
+
+        let run () = ()
 
     module execute_modules =
         let run () =
             building_strings_and_formatting_data.run()
+            parsing_strings_and_textual_data.run()
+            using_regular_exprtessions_to_parse_files.run()
 
