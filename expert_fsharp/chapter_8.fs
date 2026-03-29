55c22437eadbe8f517f88c594a8aae49d5c56565
Joshua Ewulo <jewulo@gmail.com>
2026-03-29 22:28:14 +0100

    Still On Chapter 8
    CONTINUE FROM BOTTOM OF PAGE 174.
    
    let firstAndSercondWord (inp : string) =

--- expert_fsharp/chapter_8.fs
+++ expert_fsharp/chapter_8.fs
@@ -118,18 +118,144 @@ module chapter_8
             processing_line_based_input_2()
             processing_file_based_input()
 
-    /// CONTINUE FROM PAGE 170.
-    /// USING REGULAR EXPRESSIONS TO PARSE LINES.
-    module using_regular_exprtessions_to_parse_files =
+    module using_regular_expressions_to_parse_files =
 
         open System.Text.RegularExpressions
 
+        let parseHttpRequest line = 
+            let result = Regex.Match(line, @"GET (.*?) HTTP/1\.([01])$")
+            let file = result.Groups.[1].Value
+            let version = result.Groups.[2].Value
+            file, version
 
-        let run () = ()
+        let run () =
+            let HTMLRequestPart = "GET /favicon.ico HTTP/1.1"
+            parseHttpRequest HTMLRequestPart |> printfn "%A"
+
+    module more_on_matching_with_system_text_regular_expr_1 =
+
+        open System.Text.RegularExpressions
+
+        // define a simple function to create a new Regular Expression
+        let regex s = new Regex(s)
+
+        // define Perl like operators for Regular Expressions
+        let (=~) s (re : Regex) = re.IsMatch(s)
+        let (<>~) s (re : Regex) = not (s =~ re)
+
+        // examples uses of regular expressions
+
+
+        let check_substring () =
+            let samplestring = "This is a string"
+
+            // check if samplestring contains "his"
+            if samplestring =~ regex "his" then
+                printfn "A Match! "
+
+            // check if "This is a string" contains "his"
+            "This is a string" =~ regex "(is)+" |> printfn "%A"
+
+        let split_string () =
+            // regular expressions can also be used to split string
+            regex(" ").Split("This is a string") |> printfn "%A"
+
+            // use " +" to match multiple spaces
+            regex(" ").Split("This is a     string") |> printfn "%A"
+            regex(" +").Split("This is a     string") |> printfn "%A"
+
+            // use @"\s" to match any unicode whitespace character including end-of-line markers
+            regex("\s").Split("I'm a little         teapot") |> printfn "%A"
+            regex("\s+").Split("I'm a little         teapot") |> printfn "%A"
+            regex(@"\s").Split("I'm a little         teapot") |> printfn "%A"
+            regex(@"\s+").Split("I'm a little         teapot") |> printfn "%A"
+            regex(@"\s").Split("I'm a little   \t\t\n\t\n\t  teapot") |> printfn "%A"
+            regex(@"\s+").Split("I'm a little   \t\t\n\t\n\t  teapot") |> printfn "%A"
+
+        let check_substring_position () =
+            let m = regex("joe").Match("maryjoewashere")
+
+            if m.Success then
+                m.Index |> printfn "Matched at position %d"
+
+        let replace_text () =
+            let text = "was a dark and stormy night"
+            let t2 = regex(@"\w+").Replace(text, "WORD")
+            t2 |> printfn "%A"
+
+        let replace_text_2 () =
+            let text = "was a dark and stormy night"
+            let t2 = regex("\w+").Replace(text, "WORD")
+            t2 |> printfn "%A"
+
+        let case_sensitive_check_substring () =
+            let samplestring = "This is a string"
+
+            // using regex "(?i)" for case sensitivity
+
+            // check if "This is a string" contains "his" or "HIS"
+            samplestring =~ regex "(?i)HIS" |> printfn "%A"
+
+            // check if "This is a string" contains "HIS"
+            samplestring =~ regex "HIS" |> printfn "%A"
+
+        let run () =
+            check_substring()
+            split_string()
+            check_substring_position()
+            replace_text()
+            replace_text_2()
+            case_sensitive_check_substring()
+
+    module matching_with_named_groups =
+        
+        open System.Text.RegularExpressions
+
+        // define a simple function to create a new Regular Expression
+        let regex s = new Regex(s)
+
+        let entry = @"
+        Jolly Jethro
+        13 Kings Parade
+        Cambridge, Cambs CB2 1TJ
+        "
+
+        let re = regex @"(?<=\n)\s*(?<city>[^\n]+)\s*,\s*(?<countr>\w+)\s+(?<pcode>.{3}\s*.{3}).*$"
+
+        // define an active pattern
+        let (|IsMatch|_|) (re : string) (inp : string) =
+            if Regex(re).IsMatch(inp) then Some() else None
+
+        let use_is_active_pattern (str : string) =
+            match str with
+            | IsMatch "(?i)HIS" -> "yes, it matched"
+            | IsMatch "ABC" -> "this will not match"
+            | _ -> "nothing matched"
+
+
+    /// CONTINUE FROM BOTTOM OF PAGE 174.
+
+    // let firstAndSercondWord (inp : string) =
+
+        let run () =
+            let r = re.Match(entry)
+            r |> printfn "%A"
+            r.Groups.["city"].Value   |> printfn "%A"
+            r.Groups.["county"].Value |> printfn "%A"
+            r.Groups.["pcode"].Value  |> printfn "%A"
+
+            use_is_active_pattern("This is a string") |> printfn "%s"
+            use_is_active_pattern("That is a string") |> printfn "%s"
+
+
+
+
+
 
     module execute_modules =
         let run () =
             building_strings_and_formatting_data.run()
             parsing_strings_and_textual_data.run()
-            using_regular_exprtessions_to_parse_files.run()
-
+            using_regular_expressions_to_parse_files.run()
+            more_on_matching_with_system_text_regular_expr_1.run()
+            matching_with_named_groups.run()
