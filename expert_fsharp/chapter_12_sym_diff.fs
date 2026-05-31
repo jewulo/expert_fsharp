d0d9ba545fd49d4cac529bf90feebca5b380e3d3
Joshua Ewulo <jewulo@gmail.com>
2026-05-31 19:24:34 +0100

    Update comments for chapter 12 reference clarity

--- expert_fsharp/chapter_12_sym_diff.fs
+++ expert_fsharp/chapter_12_sym_diff.fs
@@ -3,8 +3,8 @@ module chapter_12_sym_diff
     open System
 
     /// CONTINUE FROM CHAPTER 12: SYMBOLIC DIFFERENTIATION AND EXPRESSION RENDERING
-    /// EXPERT F# 3.0 :PAGE 309
-    /// EXPERT F# 4.0 :PAGE 332
+    /// EXPERT F# 3.0 :PAGE 309 - Symbolic Differentiaion and Expression Rendering 
+    /// EXPERT F# 4.0 :PAGE 332 - Expression Simplification and Differentiation
 
     module modelling_simple_algebraic_expressions =
         type Expr =
