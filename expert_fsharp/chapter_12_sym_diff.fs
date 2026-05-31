27af477ee956aafa667a47c7389cf249e10e085d
Joshua Ewulo <jewulo@gmail.com>
2026-05-31 19:14:29 +0100

    Add symbolic differentiation module structure

--- expert_fsharp/chapter_12_sym_diff.fs
+++ expert_fsharp/chapter_12_sym_diff.fs
@@ -1,1 +1,21 @@
+module chapter_12_sym_diff
+
+    open System
+
+    /// CONTINUE FROM CHAPTER 12: SYMBOLIC DIFFERENTIATION AND EXPRESSION RENDERING
+    /// EXPERT F# 3.0 :PAGE 309
+    /// EXPERT F# 4.0 :PAGE 332
+
+    module modelling_simple_algebraic_expressions =
+        type Expr =
+            | Var
+            | Num of int
+            | Sum of Expr * Expr
+            | Prod of Expr * Expr
+
+        let run () = ()
+
+    let run () = ()
+
+
 
