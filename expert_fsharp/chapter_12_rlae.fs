69db16ae46f244e5ac2703dc9c44290be1add817
Joshua Ewulo <jewulo@gmail.com>
2026-06-05 19:50:07 +0100

    Add chapter 12 module for a richer language of algebraic expressions

--- expert_fsharp/chapter_12_rlae.fs
+++ expert_fsharp/chapter_12_rlae.fs
@@ -1,1 +1,11 @@
+module chapter_12_rlae
 
+    /// CONTINUE FROM CHAPTER 12: SYMBOLIC DIFFERENTIATION AND EXPRESSION RENDERING
+    /// EXPERT F# 3.0 :PAGE 312 - A Richer Language of Algebraic Expressions
+    /// EXPERT F# 4.0 :PAGE 334 - A Richer Language of Algebraic Expressions
+
+    module a_richer_language_of_algebraic_expressions =
+        let run () = ()
+
+    module execute_modules =
+        let run () = ()
